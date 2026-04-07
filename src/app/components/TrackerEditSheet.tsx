895231c674f1d9dfe3b6241e2b442db3dd933b1c
H27films <hamzar.128@gmail.com>
2026-04-07 18:43:51 +0800

    TrackerEditSheet: replace icon with exact BottomNav RunnerIcon SVG

--- src/app/components/TrackerEditSheet.tsx
+++ src/app/components/TrackerEditSheet.tsx
@@ -137,12 +137,8 @@ const TrackerEditSheet: React.FC<Props> = ({ onClose, onSaved }) => {
             Edit Tracker
           </p>
           {/* Running man icon */}
-          <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="13" cy="4" r="1.5" fill="#000000" stroke="none" />
-            <path d="M7 22l2-6 2 2 3-5" />
-            <path d="M13.5 11.5L15 8l-3-1.5-2 3.5H7" />
-            <path d="M11 13.5l1 2.5-3 3" />
-            <path d="M15 8l2 1.5-1 3" />
+          <svg width="22" height="22" viewBox="0 0 24 24" fill="#000000" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.49 5.48c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm-3.6 13.9l1-4.4 2.1 2v6h2v-7.5l-2.1-2 .6-3c1.3 1.5 3.3 2.5 5.5 2.5v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.4-.6-1-1-1.7-1-.3 0-.5.1-.8.1l-5.2 2.2v4.7h2v-3.4l1.8-.7-1.6 8.1-4.9-1-.4 2 7 1.4z"/>
           </svg>
         </div>
 
